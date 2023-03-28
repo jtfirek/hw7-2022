57fe075b721d1e83168a0c10c461e0a2e6b3b64e
Jacob Firek <jacob@interlay.io>
2023-03-28 17:14:32 -0400

    changes

--- js/video.js
+++ js/video.js
@@ -1,90 +1,54 @@
-const video = document.querySelector("#player1");
-const playBtn = document.querySelector("#play");
-const pauseBtn = document.querySelector("#pause");
-const slowerBtn = document.querySelector("#slower");
-const fasterBtn = document.querySelector("#faster");
-const skipBtn = document.querySelector("#skip");
-const slider = document.querySelector("#slider");
-const volumeDisplay = document.querySelector("#volume");
-const muteBtn = document.querySelector("#mute");
-const vintageBtn = document.querySelector("#vintage");
-const origBtn = document.querySelector("#orig");
+var video = document.querySelector("#player1");
 
-function onWindowLoad() {
-	console.log("Good job opening the window");
-	video.autoplay = false;
-	video.loop = false;
-	console.log("Auto play is set to " + video.autoplay);
-	console.log("Loop is set to " + video.loop);
+window.addEventListener("load", function() {
+video.autoplay = false;
+video.loop = false;
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+var vid = document.querySelector("#player1");
+document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+vid.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+video.playbackRate *= 0.9;
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+video.playbackRate /= 0.9;
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+video.currentTime += 10;
+if (video.currentTime >= video.duration) {
+video.currentTime = 0
 }
+});
 
-function onPlayClick() {
-	console.log("Play Video");
-	video.play();
-	volumeDisplay.innerHTML = (video.volume * 100) + "%";
+document.querySelector("#mute").addEventListener("click", function() {
+if (video.muted == false) {
+video.muted = true;
+this.innerHTML = "Unmute"
+} else {
+video.muted = false
+this.innerHTML = "Mute"
 }
+});
 
-function onPauseClick() {
-	console.log("Pause Video");
-	video.pause();
-}
+document.querySelector("#slider").addEventListener("input", function () {
+video.volume = (this.value / 100);
+document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
 
-function onSlowerClick() {
-	console.log("Slow Down Video");
-	video.playbackRate *= 0.9;
-	console.log("Speed is " + video.playbackRate);
-}
+document.querySelector("#vintage").addEventListener("click", function() {
+document.querySelector("#player1").classList.add("oldSchool")
+});
 
-function onFasterClick() {
-	console.log("Speed Up Video");
-	video.playbackRate /= 0.9;
-	console.log("Speed is " + video.playbackRate);
-}
-
-function onSkipClick() {
-	console.log("Skip Ahead");
-	video.currentTime += 10;
-	if (video.currentTime >= video.duration) {
-		video.currentTime = 0;
-	}
-	console.log("Video current time is " + video.currentTime);
-}
-
-function onSliderChange() {
-	console.log("The current volume is " + video.volume);
-	video.volume = slider.value / 100;
-	volumeDisplay.innerHTML = (video.volume * 100) + "%";
-	console.log("The current volume is " + video.volume);
-}
-
-function onMuteClick() {
-	console.log("The video is muted");
-	if (video.muted === false) {
-		video.muted = true;
-		muteBtn.textContent = "Unmute";
-	} else {
-		video.muted = false;
-		muteBtn.textContent = "Mute";
-	}
-}
-
-function onVintageClick() {
-	video.classList.add("oldSchool");
-	console.log("The oldSchool class is added");
-}
-
-function onOrigClick() {
-	video.classList.remove("oldSchool");
-	console.log("The oldSchool class is removed");
-}
-
-window.addEventListener("load", onWindowLoad);
-playBtn.addEventListener("click", onPlayClick);
-pauseBtn.addEventListener("click", onPauseClick);
-slowerBtn.addEventListener("click", onSlowerClick);
-fasterBtn.addEventListener("click", onFasterClick);
-skipBtn.addEventListener("click", onSkipClick);
-slider.addEventListener("change", onSliderChange);
-muteBtn.addEventListener("click", onMuteClick);
-vintageBtn.addEventListener("click", onVintageClick);
-origBtn.addEventListener("click", onOrigClick);
+document.querySelector("#orig").addEventListener("click", function() {
+document.querySelector("#player1").classList.remove("oldSchool")
+})
